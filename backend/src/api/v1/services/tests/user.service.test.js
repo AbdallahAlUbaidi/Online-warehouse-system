82201f359be0f713422801e2f0a3b6f85f66b78c
AbdallahAlUbaidi <abdallah.alubaidi111@gmail.com>
2023-11-08 18:19:55 +0300

    Migrate tests to use vitest

--- backend/src/api/v1/services/tests/user.service.test.js
+++ backend/src/api/v1/services/tests/user.service.test.js
@@ -1,26 +1,29 @@
-import { jest } from "@jest/globals";
-
+import { vi, describe, it, expect, beforeEach, afterEach } from "vitest";
 
 import {
 	createUser
 } from "../user.service.js";
 
+import UserModel from "../../models/user.model.js";
 
-jest.mock("../../models/user.model.js", () => ({
-	__esModule: true,
-	default: {
-		findOne: jest.fn(),
-		create: jest.fn(() => ({ _id: "12345" }))
-	}
-}));
 
 
 describe("Create User service", () => {
+	let create;
+	beforeEach(() => {
+		create = vi.spyOn(UserModel, "create");
+	});
+
+	afterEach(() => {
+		vi.clearAllMocks();
+	});
+
 
 	it("Should return created user given a username and a password", async () => {
 		//Arrange
 		const username = "myUsername";
 		const password = "myPassword";
+		create.mockImplementation(() => ({ _id: "12341" }));
 
 		//Act
 		const newUser = await createUser(username, password);
