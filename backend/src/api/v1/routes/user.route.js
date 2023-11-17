8fe51f900bcd0508b67f1995a7eba7b197e60724
AbdallahAlUbaidi <abdallah.alubaidi111@gmail.com>
2023-11-17 10:22:50 +0300

    Add login route

--- backend/src/api/v1/routes/user.route.js
+++ backend/src/api/v1/routes/user.route.js
@@ -1,12 +1,15 @@
 import validateResource from "../../../middleware/validateResource.js";
+import authenticate from "../../../middleware/authenticate.js";
 
 import {
-	createUserController
+	createUserController,
+	authenticateUser
 } from "../controller/user.controller.js";
 
 
 import {
-	createUserSchema
+	createUserSchema,
+	authenticateUserSchema
 } from "../schemas/user.schema.js";
 
 import express from "express";
@@ -14,6 +17,9 @@ import express from "express";
 const router = express.Router();
 
 router.post("/signup", validateResource(createUserSchema), createUserController);
+router.post("/login", validateResource(authenticateUserSchema), authenticateUser);
 
+router.get("/test-auth", authenticate, (req, res) =>
+	res.status(200).send("Successful"));
 
 export default router;
