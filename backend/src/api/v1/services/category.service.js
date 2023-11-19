bb1d11b69d97c86369a70a6e8d8f5e3135d87622
AbdallahAlUbaidi <abdallah.alubaidi111@gmail.com>
2023-11-19 21:51:25 +0300

    Add find category by name service

--- backend/src/api/v1/services/category.service.js
+++ backend/src/api/v1/services/category.service.js
@@ -2,3 +2,6 @@ import categoryModel from "../models/category.model";
 
 export const createCategory = async (categoryName, user) =>
 	categoryModel.create({ name: categoryName, user });
+
+export const findCategoryByName = async (categoryName, user) =>
+	categoryModel.findOne({ user, name: categoryName });
