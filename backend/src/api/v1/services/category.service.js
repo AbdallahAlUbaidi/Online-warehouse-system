2b01d3f3536068c55b6c7dee423608f4b7b4276a
AbdallahAlUbaidi <abdallah.alubaidi111@gmail.com>
2023-11-19 22:05:51 +0300

    Changed category to be unique within users categories

--- backend/src/api/v1/services/category.service.js
+++ backend/src/api/v1/services/category.service.js
@@ -3,5 +3,5 @@ import categoryModel from "../models/category.model";
 export const createCategory = async (categoryName, user) =>
 	categoryModel.create({ name: categoryName, user });
 
-export const findCategoryByName = async (categoryName, user) =>
-	categoryModel.findOne({ user, name: categoryName });
+export const findCategoryByNameAndUserId = async (categoryName, userId) =>
+	categoryModel.findOne({ user: userId, name: categoryName });
