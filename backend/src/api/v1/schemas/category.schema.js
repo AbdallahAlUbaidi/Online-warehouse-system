2f044b0646207d5292b780271a5b34b4ff598bfc
AbdallahAlUbaidi <abdallah.alubaidi111@gmail.com>
2023-11-23 09:28:44 +0300

    Add get category schema

--- backend/src/api/v1/schemas/category.schema.js
+++ backend/src/api/v1/schemas/category.schema.js
@@ -1,4 +1,5 @@
 import { z, object } from "zod";
+import mongoose from "mongoose";
 
 export const createCategorySchema = object({
 	body: object({
@@ -6,5 +7,14 @@ export const createCategorySchema = object({
 			.string({ required_error: "Category name is required" })
 			.max(64, "Category name is at most 64 characters long")
 			.min(1, "Category name cannot be empty")
+	})
+});
+
+
+export const getCategorySchema = object({
+	params: object({
+		categoryId: z
+			.string({ required_error: "Must specify categoryId" })
+			.refine(mongoose.isValidObjectId, "The Id provided is invalid"),
 	})
 });
