d6cfbd4f9b798ddbd5ee6c82894edba81ed3e9a2
AbdallahAlUbaidi <abdallah.alubaidi111@gmail.com>
2023-11-25 17:52:03 +0300

    Add createTransaction service

--- backend/src/api/v1/services/transaction.service.js
+++ backend/src/api/v1/services/transaction.service.js
@@ -1,4 +1,8 @@
 import transactionModel from "../models/transaction.model.js";
+import mongoose from "mongoose";
+import {
+	updateItemStock
+} from "./item.service.js";
 
 
 export const createTransaction = async ({
@@ -6,9 +10,31 @@ export const createTransaction = async ({
 	user,
 	items,
 	payment,
-}) => transactionModel.create({
-	buyerName,
-	user,
-	payment,
-	items,
-});
+}) => {
+	const session = await mongoose.startSession();
+
+	try {
+		let transactionObj;
+		await session.withTransaction(async () => {
+
+			const updateStockPromises = items
+				.map(({ itemId, quantity }) =>
+					updateItemStock(itemId, quantity));
+
+			const transactionPromise = transactionModel.create({
+				buyerName,
+				user,
+				payment,
+				items,
+			});
+
+			const result = await Promise.all([transactionPromise, ...updateStockPromises]);
+			transactionObj = result[0];
+		});
+
+		return transactionObj;
+
+	} finally {
+		session.endSession();
+	}
+};
